e43a03b0025ced06f9e4728ca0ef443ef7c648b6
fergdev <fergus.a.hewson@outlook.com>
2025-09-29 22:28:07 +1300

    conan

---
+++ engine/conanfile.py
@@ -0,0 +1,50 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, cmake_layout
+
+required_conan_version = ">=2.0"
+
+
+class QuantEngineRecipe(ConanFile):
+    name = "quant_engine"
+    version = "0.0.0"
+    package_type = "application"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("openssl/3.3.2")
+        self.requires("nlohmann_json/3.11.3")
+        self.requires(
+            "boost/1.88.0",
+            options={
+                # "boost/*:shared": False
+                #     "without_type_erasure": True,
+                # "without_log": False,
+                #     "without_test": True,
+                #     "without_timer": True,
+                #     "without_thread": True,
+                #     "without_locale": True,
+                #     "without_fiber": True,
+                #     "without_contract": True,
+                #     "without_wave": True,
+                #     "without_nowide": True,
+                #     "without_iostreams": True,
+                #     "without_graph": True,
+            },
+        )
+        self.requires(
+            "opentelemetry-cpp/1.22.0",
+            options={
+                "with_otlp_http": True,
+                "with_otlp_grpc": False,
+                "with_zipkin": False,
+            },
+        )
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
